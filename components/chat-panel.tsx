f41ec60a856b46cfe2615972ce677304fcb425e7
aditya-gaharawar <164782232+aditya-gaharawar@users.noreply.github.com>
2024-03-30 00:54:22 +0530

    Update chat-panel.tsx

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -36,24 +36,24 @@ export function ChatPanel({
 
   const exampleMessages = [
     {
-      heading: 'What are the',
-      subheading: 'trending memecoins today?',
-      message: `What are the trending memecoins today?`
+      heading: 'Write a Roast ',
+      subheading: 'on Friend ',
+      message: `Ask me for a name and then start writing roast on the friend `
     },
     {
-      heading: 'What is the price of',
-      subheading: '$DOGE right now?',
-      message: 'What is the price of $DOGE right now?'
+      heading: 'Explain Me ',
+      subheading: 'Quantam Physics',
+      message: 'explain Quantam Physics as i am New To it and Follow with throw details '
     },
     {
-      heading: 'I would like to buy',
-      subheading: '42 $DOGE',
-      message: `I would like to buy 42 $DOGE`
+      heading: 'Baat Chit',
+      subheading: 'AI ke sath ',
+      message: `hello hwo are you i am here you have a conversation with you don't forget the sarcasm `
     },
     {
-      heading: 'What are some',
-      subheading: `recent events about $DOGE?`,
-      message: `What are some recent events about $DOGE?`
+      heading: 'Trace iconic fashion pieces',
+      subheading: `from their origins`,
+      message: `Can you help me trace the history behind iconic fashion pieces and explore their modern counterparts? Start by asking me which fashion item's past fascinates me the most.`
     }
   ]
 
