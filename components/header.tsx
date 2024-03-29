6c548123fd2886e117d28a2c8e289380908b3d9f
aditya-gaharawar <164782232+aditya-gaharawar@users.noreply.github.com>
2024-03-29 23:13:39 +0530

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,19 +1,13 @@
-now from this import * as React from 'react'
-import Link from 'next/link'
-import { cn } from '@/lib/utils'
-import { auth } from '@/auth'
-import { Button, buttonVariants } from '@/components/ui/button'
-import {
-IconGitHub,
-IconNextChat,
-IconSeparator,
-IconVercel
-} from '@/components/ui/icons'
-import { UserMenu } from '@/components/user-menu'
-import { SidebarMobile } from './sidebar-mobile'
-import { SidebarToggle } from './sidebar-toggle'
-import { ChatHistory } from './chat-history'
-import { Session } from '@/lib/types'
+now from this import * as React from 'react';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import { auth } from '@/auth';
+import { Button, buttonVariants } from '@/components/ui/button';
+import { UserMenu } from '@/components/user-menu';
+import { SidebarMobile } from './sidebar-mobile';
+import { SidebarToggle } from './sidebar-toggle';
+import { ChatHistory } from './chat-history';
+import { Session } from '@/lib/types';
 async function UserOrLogin() {
 const session = (await auth()) as Session
 return (
